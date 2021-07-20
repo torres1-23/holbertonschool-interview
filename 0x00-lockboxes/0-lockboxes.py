5888af26c801e2ffad5946f147727cb872d4e65e
torres1-23 <alejo.torres1247@gmail.com>
2021-07-20 03:16:51 +0000

    Add lockboxes añgprithm solution

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
+"""Module that performs the locboxes algrithm"""
+
 
 def canUnlockAll(boxes):
-  i = 0
-  total = list(set(boxes[0]) | {0})
-  added = True
-  while added:
-    added = False
-    for j in join(boxes, total[i:]):
-      if j not in total:
-        total.append(j)
-        i += 1
-        added= True
-  return len(total)==len(boxes)
+    """Checks if lockboxes in a list can be opened"""
+    i = 0
+    keys = list(set(boxes[0]) | {0})
+    added = True
+    while added:
+        added = False
+        for j in join(boxes, keys[i:]):
+            if j not in keys:
+                keys.append(j)
+                i += 1
+                added = True
+    return len(keys) == len(boxes)
 
-def join(T, R):
-  res = []
-  for e in R:
-    res += T[e]
-  return res
+
+def join(boxes, keys):
+    """Return number of locboxes that can be opened"""
+    box = []
+    for key in keys:
+        box += boxes[key]
+    return box
