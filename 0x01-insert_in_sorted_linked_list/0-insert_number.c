5733419ce7f1b3d1d4f79da7194ef6323e897346
torres1-23 <alejo.torres1247@gmail.com>
2021-07-26 16:11:23 +0000

    Add scripts that insert a new node in a sorted singly linked list

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_node - nserts a number into a sorted singly linked list.
+ * @head: pointer to head of list
+ * @number: number to insert.
+ * Return: new inserted node or NULL if failed.
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *nodes = *head, *new_node;
+
+	if (!head)
+		return (NULL);
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = number;
+	if (!*head)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	while (nodes->next && number > nodes->next->n)
+	{
+		nodes = nodes->next;
+	}
+	new_node->next = nodes->next;
+	nodes->next = new_node;
+	return (new_node);
+}
